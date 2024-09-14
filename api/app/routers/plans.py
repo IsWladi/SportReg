3b470387b8e8596d24bd496a95fea46bfd06c4ca
IsWladi <isaacx3984@outlook.com>
2024-09-14 17:14:24 -0300

    feat: add endpoint to schedule again a plan

--- api/app/routers/plans.py
+++ api/app/routers/plans.py
@@ -3,6 +3,8 @@ from fastapi import HTTPException, APIRouter
 # import utilities for the mongo database
 from bson.json_util import dumps
 import json
+import datetime
+import pytz
 
 #import models
 from app.models.work_out import Plan
@@ -48,3 +50,41 @@ async def get_last_completed_plan(db: db_dependency, current_user: str):
             return {"last_plan": json.loads(dumps(plan))}
 
     return {"message": "There are no completed plans."}
+
+
+@router.post("/schedule/again/last/completed/plan", status_code=200)
+async def schedule_again_last_completed_plan(db: db_dependency, current_user: str, date: datetime.datetime = datetime.datetime.now(tz=pytz.timezone('America/Santiago')).strftime('%Y-%m-%d')):
+    """
+    Schedule again the last completed plan for the current user. the date by default is the current date. Also, the comments are not included in the scheduled plan.
+    """
+    users_collection = db[current_user]
+    last_plan = users_collection.find({"type": "plan"}).sort("date", -1).limit(1)
+    last_plan = json.loads(dumps(last_plan))[0]
+
+    # validate if the last plan is completed
+    completed = True
+    for plan_workout in last_plan["plan"]:
+        if plan_workout["completed"] == False:
+            completed = False
+            break
+
+    if not completed:
+        raise HTTPException(status_code=400, detail="There are no completed plans.")
+
+    # build the scheduled plan
+    scheduled_plan = {}
+    scheduled_plan["date"] = date
+    scheduled_plan["plan"] = last_plan["plan"]
+    scheduled_plan["type"] = "plan"
+
+    # delete "comments" from the exercises in the plan
+    for exercise in scheduled_plan["plan"]:
+        # validate if the key exists
+        if "comments" in exercise:
+            del exercise["comments"]
+        # set the completed key to False
+        exercise["completed"] = False
+
+    response = users_collection.insert_one(scheduled_plan)
+    return {"message": "Plan scheduled successfully.",
+            "plan_id": str(response.inserted_id)}
